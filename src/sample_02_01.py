1f2779f44bedbd4164c66120035dffad5df3d6dd
KinjiKawaguchi <kawakin0310@icloud.com>
2022-08-17 15:17:28 +0900

    文字コードを変換しながらWebページを取得する

--- src/sample_02_01.py
+++ src/sample_02_01.py
@@ -1,2 +1,10 @@
-print("helloworld")
-print("hogehoge")
+#文字コードを変換しながらWebページを取得する
+import urllib.request
+
+if __name__ == '__main__':
+    url = 'https://ja.wikipedia.org/wiki/%E6%97%A5%E6%9C%AC'
+    with urllib.request.urlopen(url) as res:
+        byte = res.read()
+        #文字コード変換
+        html = byte.decode('utf-8')#Unicodeからstr型に変換
+        print(html)
