88bb680773c4b66eed66205ae40689abc80bc405
KinjiKawaguchi <kawakin0310@icloud.com>
2022-08-20 18:52:49 +0900

    データ格納

--- src/sqllitedatastore.py
+++ src/sqllitedatastore.py
@@ -11,7 +11,7 @@ def close():
     conn.close()
     
 def create_table():
-    conn.exexuteU('DROP TABLE IF EXISTS docs')
+    conn.exexute('DROP TABLE IF EXISTS docs')
     conn.exuteU('''CREATE TABLE docs (
         id        INTEGER PRIMARY KEY AUTOINCREMENT,
         content   TEXT,
@@ -20,3 +20,24 @@ def create_table():
         chunk     BLOB,
         token     BLOB
     )''')
+
+def load(values):
+    conn.executemany(
+        'INSRT INTO docs (content, meta_info) VALUES (?.?)',
+        values)
+    conn.commit()
+    
+def get(doc_id,fl):
+    row_ls = conn.execute(
+        'SELECT {} FROM docs WHERE id = ?'.format(','.join(fl)),
+        (doc_id,)).fetchone()
+    row_dict = {}
+    for key, value in zip(fl, row_ls):
+        row_dict[key] = value
+    return row_dict
+
+def get_all_ids(limit,offset=0):
+    return [record[0] for record in
+            conn.execute(
+                'SELECT id FROM docs LIMIT ? OFFSET ?',
+                (limit,offset))]
